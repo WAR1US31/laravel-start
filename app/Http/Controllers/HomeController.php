84bb1740825e776e0af1339ce535259e31bd154a
Dimon <dima@webdima31.ru>
2023-12-17 15:49:05 +0300

    Stage 7 done!

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+
+use App\Models\User;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 
 class HomeController extends Controller
 {
-    //
+    public function __invoke(): Factory|View|Application
+    {
+        $users = User::query()
+            ->select('id', 'name', 'email')
+            ->paginate(3);
+
+        return view('home', compact('users'));
+    }
 }
